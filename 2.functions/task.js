6f66fcc343710c415e944d3bcf389e42df5fa2a0
Arti <Artyouha@yandex.ru>
2022-11-08 22:55:16 +0200

    fdsf

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,50 @@
 // Задание 1
 function getArrayParams(arr) {
+ let arr = []
   let min, max, sum, avg;
-
-  // Ваш код
-
+min = Infinity max = -Infinity;
+max = Infinity min = Infinity;
+sum = min + max;
+for(let i = 0; i < arr.length; i++){
+if(i > max){
+max = i;
+}else if(i < min){
+min = i
+}
+ } avg = (sum+i)/3 .toFixed(2)
+let num = Number(avg)
   return { min: min, max: max, avg: avg };
 }
 
+
 // Задание 2
 function worker(arr) {
-  let sum;
-
-  // Ваш код
-
+  let sum = 0;
+  for(let i = 0; i < arr.length; i++){
+    sum += arr[i]
+  }
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
-
-  // Ваш кода
-  // for ...
-  
+  let max = 0;
+let func = worker(arr)
+arrOfArr=[[1, 2, 3, 4], [10, 20, -10, -20]]
+for(let i = 0; i < arrOfArr.length; i++){
+  func(arrOfArr[i]);
+  if(max < sum){
+    max = sum
+  }
+}
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  let sum = 0;
+  for(let i = 0; i < arr.length; i++){
+    sum -= arr[i]
+  }
+  Math.abs(sum)
+  return sum;
 }
